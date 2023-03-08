1c8d112af80e596a5f180de37792074b2a543db4
eliottguillossou <eliott.guillossou@gmail.com>
2023-03-08 12:33:13 +0100

    Added tables for articles and author

--- creation.sql
+++ creation.sql
@@ -1,3 +1,31 @@
 drop schema if exists appli_dblp cascade;
 create schema appli_dblp;
 set schema 'appli_dblp';
+
+CREATE TABLE _article(
+    id_dblp INTEGER PRIMARY KEY,
+    type VARCHAR(1000) not null,
+    doi VARCHAR(255) not null,
+    title VARCHAR(1000) not null,
+    venue VARCHAR(100) not null,
+    year date not null,
+    pages VARCHAR(20) not null,
+    ee VARCHAR(1000) not null,
+    url_dblp VARCHAR(1000) not null
+);
+
+CREATE TABLE _author(
+    id_author INTEGER PRIMARY KEY,
+    name VARCHAR(255) not null,
+    last_name VARCHAR(255) not null
+);
+
+CREATE TABLE _written_by(
+    id_dblp INTEGER not null,
+    id_author INTEGER not null,
+    PRIMARY KEY (id_dblp, id_author),
+    FOREIGN KEY (id_dblp) REFERENCES _article(id_dblp),
+    FOREIGN KEY (id_author) REFERENCES _author(id_author)
+);
+
+
