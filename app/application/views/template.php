6864944c8766903a8ed8727f2682f3b208d80cdf
aspenne <axel.mch@outlook.fr>
2023-04-08 19:16:34 +0200

    add class to accordate with the style default

--- app/application/views/template.php
+++ app/application/views/template.php
@@ -27,7 +27,7 @@
 <body>
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">DBLP VIZ</a>
+      <a class="navbar-brand" href="/app">DBLP VIZ</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -52,8 +52,8 @@
     </div>
   </div>
   
-  <footer>
-    <div class="container">
+  <footer class="bg-dark";>
+    <div class="container ">
       <div class="row">
         <div class="col-2">
           <p>&copy;2022 DBLP VIZ</p>
